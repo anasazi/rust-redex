de228e60fd46d91361315b99c89c3e3f9016860c
Eric Reed <ecreed@cs.washington.edu>
2014-04-03 16:15:46 -0700

    coverting some things to metafunctions

--- typed.rkt
+++ typed.rkt
@@ -44,6 +44,41 @@
 
   )
 
+;; EQUALITY OPERATIONS
+
+'x-≠
+(define-relation
+  patina
+  x-≠ ⊆ x × x
+  [(x-≠ x_!_0 x_!_0)])
+
+(redex-check patina (x_0 x_1) (equal? (not (equal? (term x_0) (term x_1))) 
+				      (term (x-≠ x_0 x_1))))
+
+(test-equal #t (term (x-≠ x y)))
+(test-equal #f (term (x-≠ x x)))
+
+'τ-=
+(define-relation
+  patina
+  τ-= ⊆ τ × τ
+  [(τ-= int int)]
+  )
+
+(test-equal #t (term (τ-= int int)))
+
+'lv-≠
+(define-relation
+  patina
+  lv-≠ ⊆ lv × lv
+  [(lv-≠ x_0 x_1)
+   (x-≠ x_0 x_1)]
+  )
+
+(redex-check patina (lv_0 lv_1) (equal? (not (equal? (term lv_0) (term lv_1))) 
+					(term (lv-≠ lv_0 lv_1))))
+
+
 ;;;; EVALUATION
 'patina-machine
 (define-extended-language patina-machine patina
@@ -74,7 +109,6 @@
 (test-equal #f (term (α-= (0 0) (1 0))))
 (test-equal #f (term (α-= (0 0) (0 1))))
 (test-equal #f (term (α-= (0 0) (1 1))))
-(test-results)
 
 'α-offset
 (define-metafunction
@@ -98,7 +132,6 @@
 (test-equal (term (H-in (((0 0) (int 0)) ((1 1) ⊥)) (0 0))) #t)
 (test-equal (term (H-in (((0 0) (int 0)) ((1 1) ⊥)) (1 1))) #t)
 (test-equal (term (H-in (((0 0) (int 0)) ((1 1) ⊥)) (0 1))) #f)
-(test-results)
 
 'H-get
 (define-metafunction
@@ -108,7 +141,6 @@
 
 (test-equal (term (H-get (((0 0) (int 0)) ((1 1) ⊥)) (0 0))) '(int 0))
 (test-equal (term (H-get (((0 0) (int 0)) ((1 1) ⊥)) (1 1))) '⊥)
-(test-results)
 
 'H-set
 (define-metafunction
@@ -127,7 +159,6 @@
 (test-equal
   (term (H-set (((0 0) (int 0)) ((1 1) ⊥)) (1 1) (int 1)))
   '(((0 0) (int 0)) ((1 1) (int 1))))
-(test-results)
 
 ; H-get respects H-set
 (redex-check patina-machine ((h ...) α hv)
@@ -150,7 +181,6 @@
 (test-equal
   (term (H-del (((0 0) (int 0)) ((1 1) ⊥)) (1 1)))
   '(((0 0) (int 0))))
-(test-results)
 
 'H-cons
 (define-metafunction
@@ -183,7 +213,6 @@
 (test-equal (term (V-in ((x (0 0)) (y (1 1))) x)) #t)
 (test-equal (term (V-in ((x (0 0)) (y (1 1))) y)) #t)
 (test-equal (term (V-in ((x (0 0)) (y (1 1))) z)) #f)
-(test-results)
 
 'V-get
 (define-metafunction
@@ -193,7 +222,6 @@
 
 (test-equal (term (V-get ((x (0 0)) (y (1 1))) x)) '(0 0))
 (test-equal (term (V-get ((x (0 0)) (y (1 1))) y)) '(1 1))
-(test-results)
 
 'V-set
 (define-metafunction
@@ -212,7 +240,6 @@
 (test-equal
   (term (V-set ((x (0 0)) (y (1 1))) y (1 0)))
   '((x (0 0)) (y (1 0))))
-(test-results)
 
 ; V-get respects V-set
 (redex-check patina-machine ((v ...) x α)
@@ -235,7 +262,6 @@
 (test-equal
   (term (V-del ((x (0 0)) (y (1 1))) y))
   '((x (0 0))))
-(test-results)
 
 'V-cons
 (define-metafunction
@@ -259,7 +285,6 @@
   )
 
 (test-equal (term (sizeof int)) '1)
-(test-results)
 
 'alloc
 (define-judgment-form
@@ -284,7 +309,6 @@
      (((1 0) ⊥) ((0 0) (int 0)))
      ((y (1 0)) (x (0 0)))
      )))
-(test-results)
 
 ; make sure alloc works
 (redex-check patina-machine (H_0 V_0 x τ)
@@ -310,7 +334,6 @@
     (E-lv () ((a (0 0)) (d (12 321)) (n (123 12321))) d α)
     α)
   '((12 321)))
-(test-results)
 
 'E-rv
 (define-judgment-form
@@ -340,7 +363,6 @@
   '(((((0 0) (int 1)) ((1 0) (int 2)))
      ((a (0 0)) (b (1 0)))
      (int 3))))
-(test-results)
 
 'patina-step
 (define patina-step
@@ -411,7 +433,6 @@
   (term (() () ((block ((x : int) (y : int)) ((delete y) (delete x))))))
   (term (() () ())))
 
-(test-results)
 
 
 ;;;; TYPING
@@ -423,6 +444,12 @@
   (Γ (γ ...))
   ;; list of initialized paths
   (I (lv ...))
+  ;; heap types
+  (hτ (ptr hτ)
+      int
+      ⊥)
+  (σ (α hτ))
+  (Σ (σ ...))
   )
 
 'vd→γ
@@ -445,24 +472,27 @@
 (test-equal
   (term (Γ-extend () ((x : int) (y : int) (z : int) (x : int))))
   (term ((x int) (z int) (y int) (x int))))
-(test-results)
 
-'x-≠
-(define-judgment-form
-  patina
-  #:mode (x-≠ I I)
-  #:contract (x-≠ x x)
-  [----------------- "x-≠"
-   (x-≠ x_!_0 x_!_0)
-   ]
-  )
+'Γ-in
+(define-relation
+  patina-context
+  Γ-in ⊆ Γ × x
+  [(Γ-in ((x_0 _) _ ...) x_0)]
+  [(Γ-in ((x_0 _) γ ...) x_1)
+   (Γ-in (γ ...) x_1)])
 
-(redex-check patina-context (x_0 x_1) (equal? (not (equal? (term x_0) (term x_1)))
-					      (term (x-≠ x_0 x_1))))
+(test-equal #t (term (Γ-in ((x int) (y int)) x)))
+(test-equal #t (term (Γ-in ((x int) (y int)) y)))
+(test-equal #f (term (Γ-in ((x int) (y int)) z)))
 
-(test-equal #t (judgment-holds (x-≠ x y)))
-(test-equal #f (judgment-holds (x-≠ x x)))
-(test-results)
+'Γ-get
+(define-metafunction
+  patina-context
+  Γ-get : Γ x -> τ
+  [(Γ-get Γ x) ,(cadr (assoc (term x) (term Γ)))])
+
+(test-equal (term (Γ-get ((x int)) x)) 'int)
+(test-equal (term (Γ-get ((y int) (x int)) x)) 'int)
 
 'Γ-use
 (define-judgment-form
@@ -508,52 +538,37 @@
     (Γ-use () x Γ)
     Γ)
   '())
-(test-results)
 
-'Γ-get
-(define-judgment-form
-  patina-context
-  #:mode (Γ-get I I O)
-  #:contract (Γ-get Γ x τ)
-  [----------------------------- "Γ-get-here"
-   (Γ-get ((x_0 τ) γ ...) x_0 τ)
-   ]
-  [(Γ-get (γ ...) x_1 τ_1) 
-   --------------------------------- "Γ-get-there"
-   (Γ-get ((x_0 τ_0) γ ...) x_1 τ_1)
-   ]
-  )
+;'Γ-get
+;(define-judgment-form
+;  patina-context
+;  #:mode (Γ-get I I O)
+;  #:contract (Γ-get Γ x τ)
+;  [----------------------------- "Γ-get-here"
+;   (Γ-get ((x_0 τ) γ ...) x_0 τ)
+;   ]
+;  [(Γ-get (γ ...) x_1 τ_1) 
+;   --------------------------------- "Γ-get-there"
+;   (Γ-get ((x_0 τ_0) γ ...) x_1 τ_1)
+;   ]
+;  )
+;
+;(test-equal
+;  (judgment-holds
+;    (Γ-get ((x int)) x τ)
+;    τ)
+;  '(int))
+;(test-equal
+;  (judgment-holds
+;    (Γ-get ((y int) (x int)) x τ)
+;    τ)
+;  '(int))
+;(test-equal
+;  (judgment-holds
+;    (Γ-get () x τ)
+;    τ)
+;  '())
 
-(test-equal
-  (judgment-holds
-    (Γ-get ((x int)) x τ)
-    τ)
-  '(int))
-(test-equal
-  (judgment-holds
-    (Γ-get ((y int) (x int)) x τ)
-    τ)
-  '(int))
-(test-equal
-  (judgment-holds
-    (Γ-get () x τ)
-    τ)
-  '())
-(test-results)
-
-
-'τ-=
-(define-judgment-form
-  patina
-  #:mode (τ-= I I)
-  #:contract (τ-= τ τ)
-  [------------- "τ-=-int-int"
-   (τ-= int int)
-   ]
-  )
-
-(test-equal #t (judgment-holds (τ-= int int)))
-(test-results)
 
 'Γ-⊆
 (define-judgment-form
@@ -563,7 +578,8 @@
   [---------- "Γ-⊆-∅"
    (Γ-⊆ () Γ)
    ]
-  [(Γ-get Γ_1 x τ_1) ; make sure x is in Γ_1 ...
+  [(Γ-in Γ_1 x)
+   (where τ_1 (Γ-get Γ_1 x)) ; make sure x is in Γ_1 ...
    (τ-= τ_0 τ_1)     ; ... as has the type we expect (i.e. τ_0)
    (Γ-use Γ_1 x Γ_2) ; remove x from Γ_1 ...
    (Γ-⊆ (γ ...) Γ_2) ; ... and recurse
@@ -580,7 +596,6 @@
 ;(test-equal #f (judgment-holds (Γ-⊆ ((x int) (x int)) ((x int)))))
 ;(test-equal #t (judgment-holds (Γ-⊆ ((x int) (x int)) ((x int) (x int)))))
 (test-equal #f (judgment-holds (Γ-⊆ ((z int)) ((x int)))))
-(test-results)
 
 'initialized
 (define-judgment-form
@@ -599,20 +614,6 @@
 (redex-check patina-context (lv_0 lv_1 ...)
   (equal? (if (member (term lv_0) (term (lv_1 ...))) #t #f)
 	  (judgment-holds (initialized (lv_1 ...) lv_0))))
-
-'lv-≠
-(define-judgment-form
-  patina-context
-  #:mode (lv-≠ I I)
-  #:contract (lv-≠ lv lv)
-  [(x-≠ x_0 x_1)
-   -------------- "lv-≠-var"
-   (lv-≠ x_0 x_1)
-   ]
-  )
-
-(redex-check patina-context (lv_0 lv_1) (equal? (not (equal? (term lv_0) (term lv_1)))
-						(term (lv-≠ lv_0 lv_1))))
 
 'uninitialized
 (define-judgment-form
@@ -704,14 +705,14 @@
 ;(test-equal #f (judgment-holds (I-⊆ (x x) (x)))))
 ;(test-equal #t (judgment-holds (I-⊆ (x x) (x x))))
 (test-equal #f (judgment-holds (I-⊆ (z) (x))))
-(test-results)
 
 'τ-lv
 (define-judgment-form 
  patina-context
  #:mode (τ-lv I I O)
  #:contract (τ-lv Γ lv τ)
- [(Γ-get Γ x τ)
+ [(Γ-in Γ x)
+  (where τ (Γ-get Γ x))
   ------------- "τ-lv-var"
   (τ-lv Γ x τ)
   ]
@@ -732,7 +733,6 @@
     (τ-lv () x τ)
     τ)
   '())
-(test-results)
 
 'τ-rv
 (define-judgment-form 
@@ -769,7 +769,6 @@
    (τ-rv ((x int) (y int)) (y) (x + y) τ I)
    (τ I))
  '())
-(test-results)
 
 ; addition preserves initialization
 (redex-check patina-context (x_0 x_1)
@@ -867,7 +866,6 @@
     (τ-blk () () (block ((x : int)) ((x = 1))) I)
     I)
   '())
-(test-results)
 
 ;(define-union-language patina-context-machine patina-context patina-machine)
 
@@ -902,3 +900,4 @@
 ;
 ;(define (reduces? sts)
 
+(test-results)
